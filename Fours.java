351b097194d5442d307d9cd0455a466714c83fc9
SanjanaRaiBS <sanjanaraibs11@gmail.com>
2024-11-16 14:59:23 +0530

    sum of three numbers

--- Fours.java
+++ Fours.java
@@ -3,7 +3,7 @@ Sum of Three Numbers in itself is like Adding Two Numbers Two Times.*/
 
 import java.util.Scanner;
 
-public class Four {
+public class Fours {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter three numbers");
