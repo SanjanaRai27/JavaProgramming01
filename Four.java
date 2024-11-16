a4a8361c348aee1cae8319558eab81ce67380fc9
SanjanaRaiBS <sanjanaraibs11@gmail.com>
2024-11-16 14:51:31 +0530

    java sum program of three numbers

---
+++ Four.java
@@ -0,0 +1,24 @@
+/*Sum of Three Numbers in Java
+Sum of Three Numbers in itself is like Adding Two Numbers Two Times.*/
+
+import java.util.Scanner;
+
+public class Four {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter three numbers");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+    
+        int k=a+b;
+        int result=k+c;
+        System.out.println("sum:"+result);
+        System.out.println("sum another way"+(a+b+c));
+    
+        
+    }
+        
+    }
+    
+
